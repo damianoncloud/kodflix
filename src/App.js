de959d7aa78d7b0676fa201b873c89bc29931df7
Damiano Del Bello <dam6dam@gmail.com>
2020-11-11 17:30:22 +0100

    Add 6 Tv show titles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
+import bloodDiamond from './Blood-diamonds.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix!!!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Kodflix!</h1>
+        <img src={bloodDiamond} alt="Blood Diamonds Movie Cover" />
+        <br /><br /><br />
+        <div className="container">
+          <div className="item"><h2>Blood Diamonds</h2></div>
+          <div className="item"><h2>Chocolate</h2></div>
+          <div className="item"><h2>Parasite</h2></div>
+        </div>
+        <div className="container">
+          <div className="item"><h2>Titanic</h2></div>
+          <div className="item"><h2>Enemy of the State</h2></div>
+          <div className="item"><h2>Matrix</h2></div>
+        </div>
     </div>
   );
 }
