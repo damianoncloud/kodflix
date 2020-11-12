f0fb7df3e3c54903406a161e1d1801d5b352ccd6
Damiano Del Bello <dam6dam@gmail.com>
2020-11-12 11:50:27 +0100

    Replace tv show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import bloodDiamond from './Blood-diamonds.jpg'
+import React from 'react';
+import bloodDiamond from './images/blood-diamonds-cover.jpg';
+import chocolate from './images/chocolate-cover.jpg';
+import parasite from './images/parasite-cover.jpg';
+import titanic from './images/titanic-cover.jpg';
+import enemyofstate from './images/enemy-state-cover.jpg';
+import matrix from './images/matrix-cover.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Kodflix!</h1>
-        <img src={bloodDiamond} alt="Blood Diamonds Movie Cover" />
         <br /><br /><br />
         <div className="container">
-          <div className="item"><h2>Blood Diamonds</h2></div>
-          <div className="item"><h2>Chocolate</h2></div>
-          <div className="item"><h2>Parasite</h2></div>
+          <div className="item">
+            <img src={bloodDiamond} alt="Blood Diamonds cover"/>
+          </div>
+          <div className="item">
+            <img src={chocolate} alt="Chocolate cover"/>
+          </div>
+          <div className="item">
+            <img src={parasite} alt="Parasite cover"/>
+          </div>
         </div>
         <div className="container">
-          <div className="item"><h2>Titanic</h2></div>
-          <div className="item"><h2>Enemy of the State</h2></div>
-          <div className="item"><h2>Matrix</h2></div>
+          <div className="item">
+            <img src={titanic} alt="Titanic cover"/>
+          </div>
+          <div className="item">
+            <img src={enemyofstate} alt="Enemy of State Cover"/>
+          </div>
+          <div className="item">
+            <img src={matrix} alt="Matrix cover"/>
+          </div>
         </div>
     </div>
   );
