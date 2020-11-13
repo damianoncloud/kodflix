5b4efb3c9284fd852de9352a5660ba478d5da467
Damiano Del Bello <dam6dam@gmail.com>
2020-11-13 11:37:53 +0100

    Add Tv show title overlay

--- src/App.js
+++ src/App.js
@@ -16,23 +16,41 @@ function App() {
         <div className="container">
           <div className="item">
             <img src={bloodDiamond} alt="Blood Diamonds cover"/>
+            <div className="overlay">
+              <h1>Blood Diamonds</h1>
+            </div>
           </div>
           <div className="item">
             <img src={chocolate} alt="Chocolate cover"/>
+            <div className="overlay">
+              <h1>Chocolate</h1>
+            </div>
           </div>
           <div className="item">
             <img src={parasite} alt="Parasite cover"/>
+            <div className="overlay">
+              <h1>Parasite</h1>
+            </div>
           </div>
         </div>
         <div className="container">
           <div className="item">
             <img src={titanic} alt="Titanic cover"/>
+            <div className="overlay">
+              <h1>Titanic</h1>
+            </div>
           </div>
           <div className="item">
             <img src={enemyofstate} alt="Enemy of State Cover"/>
+            <div className="overlay">
+              <h1>Enemy of State</h1>
+            </div>
           </div>
           <div className="item">
             <img src={matrix} alt="Matrix cover"/>
+            <div className="overlay">
+              <h1>Matrix</h1>
+            </div>
           </div>
         </div>
     </div>
