b475bf97004f29ad14b6b656887e428c9b92a058
Damiano Del Bello <dam6dam@gmail.com>
2020-11-15 12:26:51 +0100

    Move covers data model to a proper front-end data source

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import Movie from './Movie.js';
-import bloodDiamond from './images/blood-diamonds.jpg';
-import chocolate from './images/chocolate.jpg';
-import parasite from './images/parasite.jpg';
-import astarisborn from './images/a-star-is-born.jpg';
-import notime from './images/no-time-to-die.jpg';
-import theoryofeverything from './images/theory-of-everything.jpg';
-import matrix from './images/matrix-cover.jpg';
+import getGallery from './gallery-get.js';
 
 
 export default function Gallery() {
@@ -14,52 +8,17 @@ export default function Gallery() {
       <div>
         <h1>Welcome to Kodflix</h1>
         <div className="container">
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='noTimeToDie' title="No time to Die" image={notime} />  
-          <Movie id='theTheoryOfEverything' title="The Theory of Everything" image={theoryofeverything} />  
-          <Movie id='matrix' title="Matrix" image={matrix} /> 
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='noTimeToDie' title="No time to Die" image={notime} />  
-          <Movie id='theTheoryOfEverything' title="The Theory of Everything" image={theoryofeverything} />  
-          <Movie id='matrix' title="Matrix" image={matrix} /> 
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='noTimeToDie' title="No time to Die" image={notime} />  
-          <Movie id='theTheoryOfEverything' title="The Theory of Everything" image={theoryofeverything} />  
-          <Movie id='matrix' title="Matrix" image={matrix} /> 
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='noTimeToDie' title="No time to Die" image={notime} />  
-          <Movie id='theTheoryOfEverything' title="The Theory of Everything" image={theoryofeverything} />  
-          <Movie id='matrix' title="Matrix" image={matrix} /> 
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
-          <Movie id='noTimeToDie' title="No time to Die" image={notime} />  
-          <Movie id='theTheoryOfEverything' title="The Theory of Everything" image={theoryofeverything} />  
-          <Movie id='matrix' title="Matrix" image={matrix} /> 
-          <Movie id='bloodDiamonds' title="Blood Diamonds" image={bloodDiamond} />  
-          <Movie id='chocolate' title="Chocolate" image={chocolate} />  
-          <Movie id='starIsBorn' title="A Star is Born" image={astarisborn} />  
+          {
+            getGallery().map(movie => (
+              <Movie 
+                key={movie.id} 
+                id={movie.id} 
+                title={movie.title} 
+                image={movie.image}/>
+            ))
+          }
         </div>
       </div>
     )
 }
+
---
+++ src/gallery-get.js
@@ -0,0 +1,30 @@
+import bloodDiamond from './images/blood-diamonds.jpg';
+import chocolate from './images/chocolate.jpg';
+import parasite from './images/parasite.jpg';
+import astarisborn from './images/a-star-is-born.jpg';
+import notime from './images/no-time-to-die.jpg';
+import theoryofeverything from './images/theory-of-everything.jpg';
+import matrix from './images/matrix-cover.jpg';
+
+export default function getGallery() {
+    return [
+      { id: 'bloodDiamonds', title:'Blood Diamonds', image: bloodDiamond },
+      { id: 'chocolate', title:'Chocolate', image: chocolate },
+      { id: 'starIsBorn', title:'A Star is Born', image: astarisborn },
+      { id: 'noTimeToDie', title:'No time to Die', image: notime },
+      { id: 'theTheoryOfEverything', title:'The Theory of Everything', image: theoryofeverything },
+      { id: 'matrix', title:'Matrix', image: matrix },
+      { id: 'bloodDiamonds', title:'Blood Diamonds', image: bloodDiamond },
+      { id: 'chocolate', title:'Chocolate', image: chocolate },
+      { id: 'starIsBorn', title:'A Star is Born', image: astarisborn },
+      { id: 'noTimeToDie', title:'No time to Die', image: notime },
+      { id: 'theTheoryOfEverything', title:'The Theory of Everything', image: theoryofeverything },
+      { id: 'matrix', title:'Matrix', image: matrix },
+      { id: 'bloodDiamonds', title:'Blood Diamonds', image: bloodDiamond },
+      { id: 'chocolate', title:'Chocolate', image: chocolate },
+      { id: 'starIsBorn', title:'A Star is Born', image: astarisborn },
+      { id: 'noTimeToDie', title:'No time to Die', image: notime },
+      { id: 'theTheoryOfEverything', title:'The Theory of Everything', image: theoryofeverything },
+      { id: 'matrix', title:'Matrix', image: matrix }
+    ];
+}
