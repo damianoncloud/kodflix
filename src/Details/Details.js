019fe65b791042ea42e94e509ad9ddcf87ab86aa
Damiano Del Bello <dam6dam@gmail.com>
2020-11-15 19:42:21 +0100

    Add plot &  image to details page of each Tv Show

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import getGallery from './gallery-get.js';
-
+import getGallery from '../gallery-get.js';
+import './Details.css';
 export default class Details extends React.Component {
     
     constructor() {
@@ -27,9 +27,15 @@ export default class Details extends React.Component {
             }
             else {
                 return(
-                    <div>
-                    <h1>{this.state.gallery.title}</h1>
-                    <Link to='/'>Back to home page</Link>  
+                    <div className="Details">
+                        <h1>{this.state.gallery.title}</h1>
+                        <div className="container">
+                            <div>{this.state.gallery.details}</div>
+                            <img 
+                                src={this.state.gallery.image} 
+                                alt={this.state.gallery.title} />
+                        </div>
+                        <Link to='/'>Back to home page</Link>  
                     </div>
                 );
         }        
